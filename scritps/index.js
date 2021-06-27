6efa75bf1354bb30c5fcec7f86df0a26e100aa72
Andrey Malinin <anmal_98@mail.ru>
2021-06-28 00:43:58 +0300

    Js code fix

--- scritps/index.js
+++ scritps/index.js
@@ -1,42 +1,33 @@
-let initElements = ()=>{
-  const popup = document.querySelector('.popup');
-  const nameInput = document.querySelector('#name-input');
-  const statusInput = document.querySelector('#status-input');
-  const profileName = document.querySelector('.profile__name');
-  const statusName = document.querySelector('.profile__status');
-  const editButton = document.querySelector('.profile__edit-button');
-  const closeButton = document.querySelector('.popup__close-button');
-  const form = document.querySelector('.form');
+const popup = document.querySelector('.popup');
+const nameInput = document.querySelector('#name-input');
+const statusInput = document.querySelector('#status-input');
+const profileName = document.querySelector('.profile__name');
+const statusName = document.querySelector('.profile__status');
+const editButton = document.querySelector('.profile__edit-button');
+const closeButton = document.querySelector('.popup__close-button');
+const form = document.querySelector('.form');
 
-  const popupOpen = ()=> {
-    popup.classList.add('popup_opened');
-  }
-
-  const popupClose = ()=> {
-    popup.classList.remove('popup_opened');  
-  }
-
-  const handleEditButtonClick = ()=> {
-    popupOpen();
-    nameInput.value = profileName.textContent;
-    statusInput.value = statusName.textContent;
-  }
-
-  const handleFormSubmit = (e)=>{
-    e.preventDefault();
-    profileName.textContent = nameInput.value;
-    statusName.textContent = statusInput.value;
-    popupClose();
-  }
-
-  const handleCloseButtonClick = ()=> {
-    popupClose();
-  }
-
-  editButton.addEventListener('click', handleEditButtonClick);
-  closeButton.addEventListener('click', handleCloseButtonClick);
-  form.addEventListener('submit', handleFormSubmit);
+const popupOpen = ()=> {
+  popup.classList.add('popup_opened');
 }
 
-initElements();
+const popupClose = ()=> {
+  popup.classList.remove('popup_opened');
+}
 
+const handleEditButtonClick = ()=> {
+  popupOpen();
+  nameInput.value = profileName.textContent;
+  statusInput.value = statusName.textContent;
+}
+
+const handleFormSubmit = (e)=>{
+  e.preventDefault();
+  profileName.textContent = nameInput.value;
+  statusName.textContent = statusInput.value;
+  popupClose();
+}
+
+editButton.addEventListener('click', handleEditButtonClick);
+closeButton.addEventListener('click', popupClose);
+form.addEventListener('submit', handleFormSubmit);
